30224714a449893bc5454c349d2933e3bd1aca47
jyotisinghal <jsinghal694@gmail.com>
2019-10-17 15:26:27 +0530

    store page creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+	validates :description, :image_url, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title, uniqueness: true, allow_blank: true, :length => {minimum: 10}
+	validates_presence_of :title, message: "this is necessary"
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\z}i,
+		message: 'must be a url for GIF,JPG, or PNG image'
+	}
 end
